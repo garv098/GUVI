1332ad3a6fc290d35f798f292e4218ca0bfeac65
Garvit Tyagi <154688764+garv098@users.noreply.github.com>
2025-07-23 19:09:59 +0530

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,17 @@
+ #. Write a code to get the input in the given format and print the 
+#output in the given format.
+ #Sample Input:
+ #5 3
+ #1 2 3 4 5
+ #Sample Output:
+ #5 3
+ #1 2 3 4 5
+ #Completion Status: Completed
+ #Concepts Included:
+ #Input/Output
+ #Language Used: PYTHON 3
+
+A=input()
+B=input()
+print(A)
+print(B)
