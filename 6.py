e7d983a00ab54b8f859ef5da0e5b4d8933f05f0d
Garvit Tyagi <154688764+garv098@users.noreply.github.com>
2025-07-24 07:58:09 +0530

    Add files via upload

---
+++ 6.py
@@ -0,0 +1,13 @@
+# 7. Write a code to get the input in the given format and print the 
+#Garvit tyagi (garvittyagi098@gmail.com)
+#output in the given format
+# Sample Input:
+# guvi
+# Sample Output:
+# g u v i
+# Completion Status: Completed
+# Concepts Included:
+# Input/Outpu
+
+A=input()
+print(*A)
